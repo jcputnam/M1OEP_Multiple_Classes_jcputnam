2b624dfa2e7b5f7eb9fc6aa0f11e1e1580532199
James Putnam <jputnam1999@gmail.com>
2021-02-09 21:08:27 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//at least 2 classes
+//one method per class that is nontrivial
+//4 overloaded operators total
+//one enum
+//one struct
+//readme that answers questions from prompt sheet
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
